e481b736fce086aae0270a4ea4f5c6b16659a99c
David Stocker <coffeemetalcode@gmail.com>
2026-04-20 15:57:36 -0400

    fix unit tests

--- src/app/components/social-links/social-links.spec.ts
+++ src/app/components/social-links/social-links.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialLink } from './social-link';
+import { SocialLinks } from './social-links';
 
-describe('SocialLink', () => {
-  let component: SocialLink;
-  let fixture: ComponentFixture<SocialLink>;
+describe('SocialLinks', () => {
+  let component: SocialLinks;
+  let fixture: ComponentFixture<SocialLinks>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SocialLink],
+      imports: [SocialLinks],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(SocialLink);
+    fixture = TestBed.createComponent(SocialLinks);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
